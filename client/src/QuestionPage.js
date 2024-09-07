0228a3a5847e821037918876f48c51d8a66f9ed6
Whackalenso <63932931+Whackalenso@users.noreply.github.com>
2024-09-07 14:09:23 -0700

    callback

--- client/src/QuestionPage.js
+++ client/src/QuestionPage.js
@@ -3,27 +3,30 @@ import Question from "./components/Question.js";
 import Papa from "papaparse";
 
 export default function QuestionPage() {
+  //   const [questionData, setQuestionData] = useState([]);
   const [question, setQuestion] = useState({});
 
-  useEffect(() => {
+  function nextQuestion() {
     fetch("./questionData.csv")
       .then((response) => response.text())
       .then((response) => {
-        var questionData = Papa.parse(response).data;
+        var data = Papa.parse(response).data;
         var obj = {};
-        var questionNum =
-          Math.floor(Math.random() * (questionData.length - 1)) + 1;
-        questionData[0].forEach((key, i) => {
-          obj[key] = questionData[questionNum][i];
+        var questionNum = Math.floor(Math.random() * (data.length - 1)) + 1;
+        data[0].forEach((key, i) => {
+          obj[key] = data[questionNum][i];
         });
+        console.log(obj);
         setQuestion(obj);
       });
-  });
+  }
+
+  useEffect(nextQuestion, []);
 
   return (
     <div>
       <h1>Question Page</h1>
-      <Question question={question} />
+      <Question question={question} nextQuestion={nextQuestion} />
     </div>
   );
 }
