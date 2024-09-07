e622d88bd10da464987cb11e8edce506f8bafecc
Whackalenso <63932931+Whackalenso@users.noreply.github.com>
2024-09-07 15:08:03 -0700

    bad looking header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,9 +2,9 @@ import { ReactComponent as Logo } from "../logo.svg";
 
 export default function Header() {
   return (
-    <div className="flex justify-center items-center h-20">
-      <Logo className="absolute left-10 top-10 h-6" />
-      <div className="align-middle p-0">SATURN</div>
+    <div className="flex justify-center items-center h-20 pt-10">
+      <Logo className="absolute left-10 w-14" />
+      <div className="align-middle p-0 text-3xl">SATURN</div>
     </div>
   );
 }
