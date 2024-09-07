37d0d204af23d01681b6fff87d7f5bcdd0c2672f
Whackalenso <63932931+Whackalenso@users.noreply.github.com>
2024-09-07 13:42:48 -0700

    stuff

--- client/src/QuestionPage.js
+++ client/src/QuestionPage.js
@@ -1,27 +1,32 @@
-import {useState, useEffect} from 'react';
-import Papa from 'papaparse';
+import { useState, useEffect } from "react";
+import Papa from "papaparse";
 
 export default function QuestionPage() {
+  const [questionData, setQuestionData] = useState([]);
+  const [question, setQuestion] = useState({});
 
-    const [question, setQuestion] = useState();
-
-    useEffect(() => {
-        fetch( './questionData.csv' )
-        .then( response => response.text())
-        .then( response => {
-            Papa.parse(response, {
-                complete: function(results) {
-                    console.log("Finished:", results.data);
-                    setQuestion(results.data);
-                }}
-            );
-        })
+  function randomQuestion() {
+    var obj = {};
+    var questionNum = Math.floor(Math.random() * questionData.length - 1) + 1;
+    questionData[0].forEach((key, i) => {
+      obj[key] = questionData[questionNum][i];
     });
+    console.log(obj);
+  }
 
-    return (
-        <div>
-            <h1>Question Page</h1>
-            <p>{question}</p>
-        </div>
-    );
+  useEffect(() => {
+    fetch("./questionData.csv")
+      .then((response) => response.text())
+      .then((response) => {
+        setQuestionData(Papa.parse(response).data);
+        randomQuestion();
+      });
+  });
+
+  return (
+    <div>
+      <h1>Question Page</h1>
+      {/* <p>{question}</p> */}
+    </div>
+  );
 }
