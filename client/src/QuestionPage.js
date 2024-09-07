371a31a9c01c0caf3dc9885b6e817dadb74b8c8e
Whackalenso <63932931+Whackalenso@users.noreply.github.com>
2024-09-07 13:48:28 -0700

    random question

--- client/src/QuestionPage.js
+++ client/src/QuestionPage.js
@@ -2,24 +2,21 @@ import { useState, useEffect } from "react";
 import Papa from "papaparse";
 
 export default function QuestionPage() {
-  const [questionData, setQuestionData] = useState([]);
+  //   const [questionData, setQuestionData] = useState([]);
   const [question, setQuestion] = useState({});
-
-  function randomQuestion() {
-    var obj = {};
-    var questionNum = Math.floor(Math.random() * questionData.length - 1) + 1;
-    questionData[0].forEach((key, i) => {
-      obj[key] = questionData[questionNum][i];
-    });
-    console.log(obj);
-  }
 
   useEffect(() => {
     fetch("./questionData.csv")
       .then((response) => response.text())
       .then((response) => {
-        setQuestionData(Papa.parse(response).data);
-        randomQuestion();
+        var questionData = Papa.parse(response).data;
+        var obj = {};
+        var questionNum =
+          Math.floor(Math.random() * (questionData.length - 1)) + 1;
+        questionData[0].forEach((key, i) => {
+          obj[key] = questionData[questionNum][i];
+        });
+        setQuestion(obj);
       });
   });
 
