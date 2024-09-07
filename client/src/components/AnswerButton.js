3dad204a518e77c224e80849ca892b56d885d530
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2024-09-07 16:03:01 -0700

    make it better

--- client/src/components/AnswerButton.js
+++ client/src/components/AnswerButton.js
@@ -1,12 +1,12 @@
 export default function AnswerButton({ question, nextQuestion }) {
 
     return (
-        <div className="flex flex-col">
+        <div className="grid grid-cols-2 gap-1">
             {[question.correct, question.false_1, question.false_2, question.false_3].map(value => ({ value, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
     .map(({ value }) => value).map((answer) => {
                 return (
-                    <button className="bg-blue-100 p-5 rounded-lg text-left" onClick={nextQuestion}>{answer}</button>
+                    <button className="bg-blue-200 p-5 rounded-lg text-left" onClick={nextQuestion}>{answer}</button>
                 );
             })}
         </div>
