ffe0978b6f48eb6519905d47a6b7ffec140d8fdd
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2024-09-07 17:25:31 -0700

    Done

--- client/src/components/AnswerButton.js
+++ client/src/components/AnswerButton.js
@@ -1,19 +1,41 @@
+import { useState, useEffect } from "react";
+
+
 export default function AnswerButton({
   question,
   nextQuestion,
   submittedAnswer,
   setSubmittedAnswer,
 }) {
+
+
+
+  const [answers, setAnswers] = useState([]);
+
+  useEffect(() => {
+    console.log(question)
+    let tmp =  [question.correct, question.false_1, question.false_2, question.false_3]
+    .map((value) => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
+
+    setAnswers(tmp);
+    MathJax.typeset();
+    
+    console.log(tmp)
+  }, [question]);
+  
+  setInterval(() => {
+    MathJax.typeset();
+
+  }, 10);
+
   return (
     <div className="grid grid-cols-2 gap-1">
-      {[question.correct, question.false_1, question.false_2, question.false_3]
-        .map((value) => ({ value, sort: Math.random() }))
-        .sort((a, b) => a.sort - b.sort)
-        .map(({ value }) => value)
-        .map((answer) => {
+      {answers.map((answer) => {
           var color = "bg-blue-200";
           if (submittedAnswer === answer) {
-            if ((answer = question.correct)) {
+            if ((answer == question.correct)) {
               color = "bg-green-200";
             } else {
               color = "bg-red-200";
@@ -23,7 +45,14 @@ export default function AnswerButton({
             <button
               className={`${color} bg-blue-200 p-5 rounded-lg text-left inline-block transition-all active:scale-95`}
               onClick={(e) => {
+                // nextQuestion();
                 setSubmittedAnswer(answer);
+                if(answer == question.correct){
+                setTimeout(() => {
+                  // alert(1);
+                    nextQuestion();
+                  }, 1000);
+                }
               }}
             >
               {answer}
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="bg-blue-50 h-20 p-5 flex justify-center absolute bottom-0">
+    <div className="bg-blue-50 p-5 flex justify-center">
       <p>Footer</p>
     </div>
   );
