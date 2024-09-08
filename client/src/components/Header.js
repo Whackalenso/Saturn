3434339cab07cc5a7401da73eeeef2eb3c2a12d1
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2024-09-07 17:42:56 -0700

    Push header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,9 +2,10 @@ import { ReactComponent as Logo } from "../logo.svg";
 
 export default function Header() {
   return (
-    <div className="flex justify-center items-center h-20 pt-10">
-      <Logo className="absolute left-10 w-14" />
-      <div className="align-middle p-0 text-3xl">SATURN</div>
+    <div className="flex justify-between items-center h-20 pt-10 mx-8">
+      <img src="/logo_new.png" alt="SATURN" className="w-32 w-fit"/>
+      <img src="/hamburger.png" alt="hamburger" className="w-8"/>
+      {/* <div className="align-middle p-0 text-3xl">SATURN</div> */}
     </div>
   );
 }
