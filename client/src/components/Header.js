c40ffe3b26ae88b5b576b942383a4ff472a0561c
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2024-09-07 17:57:10 -0700

    no hamberuger

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="flex justify-between items-center h-20 pt-10 mx-8">
       <img src="/logo_new.png" alt="SATURN" className="w-32 w-fit"/>
-      <img src="/hamburger.png" alt="hamburger" className="w-8"/>
+      {/* <img src="/hamburger.png" alt="hamburger" className="w-8"/> */}
       {/* <div className="align-middle p-0 text-3xl">SATURN</div> */}
     </div>
   );
